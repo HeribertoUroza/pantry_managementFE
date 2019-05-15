c1aa575a63cdaf9d64855e5b08eb3764b0ed6fcd
ositaigwe@pursuit.org <j0h@example.com>
2019-05-14 22:07:40 -0400

    styled logout.js

--- src/containers/Logout/Logout.js
+++ src/containers/Logout/Logout.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import firebase from '../../firebase';
-import { Link,  } from 'react-router-dom';
-import logo from '../../assets/Branding/PossiblePantryLogo3.svg'
+import { Link, } from 'react-router-dom';
+
+//CSS
+import './logout.css';
+
+//ASSETS
+import logo from '../../assets/Branding/PossiblePantryLogoWhite.png'
 
 
 
@@ -13,13 +18,23 @@ export default class Logout extends React.Component {
 
   render() {
     return <>
-    <div className="container-fluid" style={{fontFamily:"monospace"}}>
-    <img src={logo} alt="logo"/>
-    <h3>We Hope You Return Soon</h3>
-    <br/>
-    <h4>Mistake...?</h4>
-    <h6><Link to="/login">Log Back In</Link></h6>
-    </div>
+      <div className="container-fluid logoutContainer">
+        <img src={logo} alt="logo" className="logoutLogo" />
+        <div className="row">
+          <div className="col"></div>
+          <div className="col text-center">
+            <div className="mt-5">
+              <h3 className="boldText logoutH3">We Hope You Return Soon</h3>
+              <br />
+              <h4 className="mt-5 mb-2 logoutH4">Mistake...?</h4>
+              <button className="btn waves-effect waves-light brown mt-5" type="submit" name="action" style={{ margin: "0 auto", borderRadius: '50px' }} onClick={this.handleSubmit}>LOGIN
+                            <i className="material-icons right">send</i>
+                              </button>
+            </div>
+          </div>
+          <div className="col"></div>
+        </div>
+      </div>
     </>
   }
 }
