aad0b0583cd73ebb009f08d6f532b91fa7d8f15f
Erika Perez <erikaperez@Erikas-Air.fios-router.home>
2019-06-03 13:45:28 -0400

    added dates for each day of the week

--- src/components/RecipeSearch/Dates.js
+++ src/components/RecipeSearch/Dates.js
@@ -1,44 +1,68 @@
-import React  from 'react';
+import React from 'react';
 import Moment from 'react-moment';
- 
+
 export default class MyComponent extends React.Component {
+
     render() {
         const date = new Date();
         const day = date.getDay();
+        const weekday_id = this.props.weekday_id;
+        console.log('weeee', weekday_id)
         let daysToAdd = 0;
         let daysToEnd = 5;
-        if(day === 0){ //sunday
-            daysToAdd =1;
-            daysToEnd =5;
+        if (day === 0) { //sunday
+            daysToAdd = 1;
+            daysToEnd = 5;
+            console.log(day)
         }
-        if(day === 2){//Tuesday
-            daysToAdd =6;
-            daysToEnd =11;
+        if (day === 2) {//Tuesday
+            daysToAdd = 6;
+            daysToEnd = 11;
         }
-        if(day === 3){//Wednesday
-            daysToAdd =5;
-            daysToEnd =10;
+        if (day === 3) {//Wednesday
+            daysToAdd = 5;
+            daysToEnd = 10;
         }
-        if(day === 4){//Thursday
-            daysToAdd =4;
-            daysToEnd =9;
+        if (day === 4) {//Thursday
+            daysToAdd = 4;
+            daysToEnd = 9;
         }
-        if(day === 5){//Friday
-            daysToAdd =3;
-            daysToEnd =8;
+        if (day === 5) {//Friday
+            daysToAdd = 3;
+            daysToEnd = 8;
         }
-        if(day === 6){//Saturday
-            daysToAdd =2;
-            daysToEnd =7;
+        if (day === 6) {//Saturday
+            daysToAdd = 2;
+            daysToEnd = 7;
         }
 
         return (
-            <div>
-                <h5>Start of the Week:</h5>
-                <Moment add={{ days: daysToAdd}}>{date}</Moment>
-                <h5>End of the Week:</h5>
-                <Moment add={{ days: daysToEnd}}>{date}</Moment>
-            </div>
-        );
-    }
+            <>
+                <div style={{ marginTop: "30px" }} >
+                    <div className='row'>
+                        <div className="col s12 m7">
+                            <h5 style={{ fontWeight: "bold" }}>Start of the week:</h5>
+                            <Moment format="dddd MMMM DD, YYYY" add={{ days: daysToAdd }}>{date}</Moment>
+                        </div>
+                        <div className="col s12 m7 hide-on-med-and-down center-align">
+                        <h1 style={{color:"white"}}>. </h1>
+                            <Moment format="dddd MMMM DD, YYYY" add={{ days: daysToAdd + 1 }}>{date}</Moment>
+                        </div>
+                        <div className="col s12 m7 hide-on-med-and-down center-align"> 
+                        <h1 style={{color:"white"}}>. </h1>                          
+                            <Moment format="dddd MMMM DD, YYYY" add={{ days: daysToAdd + 2 }}>{date}</Moment>
+                        </div>
+                        <div className="col s12 m7 hide-on-med-and-down center-align">
+                            <h1 style={{color:"white"}}>. </h1>
+                            <Moment format="dddd MMMM DD, YYYY" add={{ days: daysToAdd + 3 }}>{date}</Moment>
+                        </div>
+                        <div className="col s12 m7 right-align">
+                            <h5 style={{ fontWeight: "bold" }}>End of the week:</h5>
+                            <Moment format="dddd MMMM DD, YYYY" add={{ days: daysToEnd }}>{date}</Moment>
+                        </div>
+                    </div>
+                </div>
+            </>
+
+        )}
 }
--- src/components/RecipeSearch/DayCard.js
+++ src/components/RecipeSearch/DayCard.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-
 const DayCard = (props) => {
     const { day, } = props;
 
     return <>
         <div className="row">
             <div className="col s12 m7">
-                <div className="card">
+                <div className="card s12 m7">
                     <div className="card-image" style={{ overflow: "hidden" }}>
                         <img src="https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt='...' />
                         <span className="card-title" style={{ fontWeight: "bolder", fontSize: "28px", textShadow: "2px 2px #000000" }}>{day.weekday_name}</span>
