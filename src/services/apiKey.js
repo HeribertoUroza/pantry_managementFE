bda3ef5f48d149ff8cee5e64904a76b72beb50ca
Heriberto Uroza <heribertouroza@pursuit.org>
2019-06-10 19:18:45 -0400

    using REACT_APP_ method

--- src/services/apiKey.js
+++ src/services/apiKey.js
@@ -1,11 +1,11 @@
 const firebaseAPI = {
-    apiKey: "AIzaSyBIS8BFD8aWDObqmVMQC5FmGJePqhAbYZQ",
-    authDomain: "ppantry-d0edc.firebaseapp.com",
-    databaseURL: "https://ppantry-d0edc.firebaseio.com",
-    projectId: "ppantry-d0edc",
-    storageBucket: "ppantry-d0edc.appspot.com",
-    messagingSenderId: "1013875284248",
-    appId: "1:1013875284248:web:9b39a8c6fb6a6206"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 export { firebaseAPI }
