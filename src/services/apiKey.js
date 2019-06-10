532e451628625a325b569445d8472a5d44df7b08
Heriberto Uroza <heribertouroza@pursuit.org>
2019-06-10 17:42:56 -0400

    keys

--- src/services/apiKey.js
+++ src/services/apiKey.js
@@ -1,11 +1,11 @@
 const firebaseAPI = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
+    apiKey: "AIzaSyBIS8BFD8aWDObqmVMQC5FmGJePqhAbYZQ",
+    authDomain: "ppantry-d0edc.firebaseapp.com",
+    databaseURL: "https://ppantry-d0edc.firebaseio.com",
+    projectId: "ppantry-d0edc",
+    storageBucket: "ppantry-d0edc.appspot.com",
+    messagingSenderId: "1013875284248",
+    appId: "1:1013875284248:web:9b39a8c6fb6a6206"
 };
 
 export { firebaseAPI }
